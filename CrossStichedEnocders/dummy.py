99863c2a677cce5a61d6027abccc3f3e4b300198
ksingla025 <ksingla@whissle.ai>
2024-07-15 00:01:17 +0000

    deleted cross-stitched

--- CrossStichedEnocders/dummy.py
+++
@@ -1,6 +0,0 @@
-
-from nemo.collections.asr.modules.audio_preprocessing import AudioToMelSpectrogramPreprocessor
-
-allvars = {'sample_rate': 16000, 'normalize': 'per_feature', 'window_size': 0.025, 'window_stride': 0.01, 'window': 'hann', 'features': 80, 'n_fft': 512, 'log': True, 'frame_splicing': 1, 'dither': 1e-05, 'pad_to': 0, 'pad_value': 0.0}
-audio2melspectrogram = AudioToMelSpectrogramPreprocessor(**allvars)
-
