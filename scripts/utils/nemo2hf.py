27ffa7d17181676b5978055de821358f0b1b5f76
Karan Singla <karansingla@octo2.mod9.com>
2024-10-17 23:16:54 -0700

    pushing code

--- scripts/utils/nemo2hf.py
+++ scripts/utils/nemo2hf.py
@@ -5,9 +5,9 @@ import sys
 from huggingface_hub import HfApi, upload_file, create_repo
 
 # Define your repository ID and local directory
-repo_id = "WhissleAI/stt_hi_conformer_ctc_entities_age_dialiect_intent"
-local_dir = "/projects/whissle/experiments/hindi-hf"
-nemo_model_path = "/projects/whissle/experiments/hindi_adapter-ai4bharat/2024-10-14_13-22-40/checkpoints/hindi_adapter-ai4bharat.nemo"
+repo_id = "WhissleAI/speech-tagger_hi_ctc_meta"
+local_dir = "/projects/whissle/experiments/punjabi-hf"
+nemo_model_path = "/projects/whissle/experiments/punjabi_adapter-ai4bharat/2024-10-16_17-51-06/checkpoints/punjabi_adapter-ai4bharat.nemo"
 
 # Set your Hugging Face access token
 hf_token = sys.argv[1]
@@ -17,7 +17,7 @@ os.environ["HUGGINGFACE_HUB_TOKEN"] = hf_token
 os.makedirs(local_dir, exist_ok=True)
 
 # Copy the .nemo file to the local directory
-shutil.copy(nemo_model_path, os.path.join(local_dir, "slurp-3adapter.nemo"))
+shutil.copy(nemo_model_path, os.path.join(local_dir, "punjabi_adapter-ai4bharat.nemo"))
 
 # Ensure the repository exists on Hugging Face
 api = HfApi()
@@ -28,7 +28,7 @@ except:
 
 # Create a README file with metadata
 readme_text = """
-# This speech tagger performs transcription for Hindi, annotates key entities, predict speaker age, dialiect and intent.
+# This speech tagger performs transcription for Punjabi, annotates key entities, predict speaker age, dialiect and intent.
 
 Model is suitable for voiceAI applications, real-time and offline.
 
@@ -36,8 +36,8 @@ Model is suitable for voiceAI applications, real-time and offline.
 
 - **Model type**: NeMo ASR
 - **Architecture**: Conformer CTC
-- **Language**: English
-- **Training data**: CommonVoice, Gigaspeech
+- **Language**: Punjabi
+- **Training data**: AI4Bharat IndicVoices Punjabi V1 and V2 dataset
 - **Performance metrics**: [Metrics]
 
 ## Usage
@@ -54,7 +54,7 @@ pip install nemo_toolkit
 import nemo.collections.asr as nemo_asr
 
 # Step 1: Load the ASR model from Hugging Face
-model_name = 'WhissleAI/stt_hi_conformer_ctc_entities_age_dialiect_intent'
+model_name = 'WhissleAI/stt_pa_conformer_ctc_entities_age_dialiect_intent'
 asr_model = nemo_asr.models.EncDecCTCModel.from_pretrained(model_name)
 
 # Step 2: Provide the path to your audio file
@@ -75,8 +75,8 @@ with open(os.path.join(local_dir, "README.md"), "w") as f:
     f.write(readme_text)
     
 upload_file(
-path_or_fileobj=os.path.join(local_dir, "hindi_adapter-ai4bharat.nemo"),
-path_in_repo="hindi_adapter-ai4bharat.nemo",
+path_or_fileobj=os.path.join(local_dir, "punjabi_adapter-ai4bharat.nemo"),
+path_in_repo="punjabi_adapter-ai4bharat.nemo",
 repo_id=repo_id,
 token=hf_token
 )
