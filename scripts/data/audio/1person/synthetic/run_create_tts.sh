385cbca98454d55f04546b09e7b96f8047987b0c
zenitsu0509 <gangwarsanu8@gmail.com>
2025-05-08 06:34:18 +0000

    code reorganization

---
+++ scripts/data/audio/1person/synthetic/run_create_tts.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Array of language codes
+language_codes=("EN" "ES" "FR" "DE" "IT" "NL" "PT")
+#language_codes=("EN" "ES")
+
+# Function to run the python script for a given language
+run_script() {
+  lang=$1
+  python scripts/data/audio/synthetic/create_tts_manifest_xtts.py $lang 1
+}
+
+# Loop through each language code and run the python script in parallel (two at a time)
+for ((i = 0; i < ${#language_codes[@]}; i+=3)); do
+  # Run the script for the current language code in the background
+  run_script "${language_codes[$i]}" &
+  if ((i+1 < ${#language_codes[@]})); then
+    # Run the script for the next language code in the background if it exists
+    run_script "${language_codes[$i+1]}" &
+  fi
+  # Wait for both background processes to finish
+  wait
+done
